64be24d805fcff7044ccb4e5a43ba2bb374f6987
bouinicreations-source <bouinicreations@gmail.com>
2026-04-24 20:09:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
